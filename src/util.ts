87a71755fc3ec1b7bec40b8b4e3ecccf25868274
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2024-12-18 21:58:39 -0600

    ADD Import and export file json

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import { RecipeResult } from "./model/ApiTasty/RecipesListResponse";
+
 // Render template
 /**
  * Render the template in the HTMLElement of tye querySelector
@@ -48,4 +50,84 @@ export function appendLocalStorage<T>(key: string, data: T): void {
   const infoLocalStorage: T[] = getLocalStorage(key);
   infoLocalStorage.push(data);
   setLocalStorage(key, infoLocalStorage);
+}
+
+export function removeRecipeLocalStorage(id: number): void {
+  const data: RecipeResult[] = getLocalStorage("recipes");
+  const filteredData = data.filter((recipe) => recipe.id != id);
+  setLocalStorage("recipes", filteredData);
+}
+
+export function showNotification(
+  message: string,
+  type: "success" | "error" = "success"
+): void {
+  const container = document.getElementById("notification-container");
+  if (!container) return;
+
+  const notification = document.createElement("div");
+  notification.className = `notification ${type}`;
+  notification.textContent = message;
+
+  container.appendChild(notification);
+
+  // Eliminar la notificación después de 3 segundos
+  setTimeout(() => {
+    notification.remove();
+  }, 3000);
+}
+
+export function exportJSON(
+  data: string,
+  filename: string = "recipes_backup.json"
+): void {
+  const blob = new Blob([data], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
+
+export function importJSON<T>(): Promise<T | null> {
+  return new Promise((resolve, reject) => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".json";
+
+    input.addEventListener("change", (event) => {
+      const file = (event.target as HTMLInputElement).files?.[0];
+      if (!file) {
+        resolve(null); // Devuelve `null` si no se selecciona un archivo
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (e.target?.result) {
+          try {
+            const data = JSON.parse(e.target.result as string) as T;
+            resolve(data); // Resuelve la promesa con el JSON importado
+          } catch (error) {
+            reject(new Error("Invalid JSON file: " + (error as Error).message));
+          }
+        } else {
+          reject(new Error("No data read from file"));
+        }
+      };
+
+      reader.onerror = () => {
+        reject(new Error("Error reading file"));
+      };
+
+      reader.readAsText(file);
+    });
+
+    input.click();
+  });
 }
